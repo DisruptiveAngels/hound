7a3447a145021576de1ef55c981f405773c45c9b
Adrian Rangel <adrian.rangel@gmail.com>
2018-01-29 22:04:46 -0600

    fix migrations

--- db/migrate/20170911205516_add_whitelisted_to_owners.rb
+++ db/migrate/20170911205516_add_whitelisted_to_owners.rb
@@ -1,4 +1,4 @@
-class AddWhitelistedToOwners < ActiveRecord::Migration[5.0]
+class AddWhitelistedToOwners < ActiveRecord::Migration[4.2][5.0]
   def up
     add_column :owners, :whitelisted, :boolean, default: false, null: false
 
