f8069364012d88d8c177e715fddbcc315327d636
Adrian Rangel <adrian.rangel@gmail.com>
2018-03-02 09:56:51 -0600

    fixes

--- db/migrate/20170505220736_add_error_to_file_reviews.rb
+++ db/migrate/20170505220736_add_error_to_file_reviews.rb
@@ -1,4 +1,4 @@
-class AddErrorToFileReviews < ActiveRecord::Migration[4.2][5.0]
+class AddErrorToFileReviews < ActiveRecord::Migration[4.2]
   def change
     add_column :file_reviews, :error, :string
   end
